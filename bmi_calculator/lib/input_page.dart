bee9d668ff4dcedb9a1f2add830f5fbb3adc0c9d
Jay <jayandratodawat@gmail.com>
2022-11-15 17:09:56 +0530

    widget refactoring is done.

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -17,8 +17,54 @@ class _InputPageState extends State<InputPage> {
           'BMI CALCULATOR',
         ),
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+            child: ReusableCard(),
+          ),
+          Expanded(
+            flex: 1,
+            child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: const Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
