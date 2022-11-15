ce6cf1b5f6c4041382d8abf33fd37fce02d0899e
Jay <jayandratodawat@gmail.com>
2022-11-15 19:21:41 +0530

    input_page.dart is updated.

--- bmi_calculator/lib/input_page.dart
+++ bmi_calculator/lib/input_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const Color cardsColor = Color(0xFF1D1E33);
 
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
@@ -23,43 +27,91 @@ class _InputPageState extends State<InputPage> {
         children: [
           Expanded(
             child: Row(
-              children: [
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
                 Expanded(
-                  child: ReusableCard(const Color(0xFF1D1E33)),
+                  child: ReusableCard(
+                    cardsColor,
+                    cardChild: IconContent(FontAwesomeIcons.mars, 'MALE'),
+                  ),
                 ),
                 Expanded(
-                  child: ReusableCard(const Color(0xFF1D1E33)),
+                  child: ReusableCard(
+                    cardsColor,
+                    cardChild: IconContent(FontAwesomeIcons.venus, 'FEMALE'),
+                  ),
                 ),
               ],
             ),
           ),
-          Expanded(
-            child: ReusableCard(const Color(0xFF1D1E33)),
+          const Expanded(
+            child: ReusableCard(cardsColor),
           ),
           Expanded(
             flex: 1,
             child: Row(
-              children: [
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: const [
                 Expanded(
-                  child: ReusableCard(const Color(0xFF1D1E33)),
+                  child: ReusableCard(cardsColor),
                 ),
                 Expanded(
-                  child: ReusableCard(const Color(0xFF1D1E33)),
+                  child: ReusableCard(cardsColor),
                 ),
               ],
             ),
           ),
+          Container(
+            color: const Color(0xFFEB1555),
+            margin: const EdgeInsets.only(top: 10.0),
+            width: double.infinity,
+            height: bottomContainerHeight,
+          )
         ],
       ),
     );
   }
 }
 
+class IconContent extends StatelessWidget {
+  const IconContent(this.icon, this.text, {super.key});
+
+  final IconData icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Icon(
+          icon,
+          size: 60,
+        ),
+        const SizedBox(
+          height: 15.0,
+        ),
+        Text(
+          text,
+          style: const TextStyle(
+            fontSize: 15.0,
+            color: Color(0xFF8D8E98),
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-  const ReusableCard(this.colour, {super.key});
+  const ReusableCard(this.colour, {super.key, this.cardChild});
 
   final Color
       colour; //final keyword added to make colour immutable i.e it can not be changed once assigned.
+
+  final Widget? cardChild;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -68,6 +120,7 @@ class ReusableCard extends StatelessWidget {
         color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
+      child: cardChild,
     );
   }
 }
